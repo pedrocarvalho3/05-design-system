da2f9ae41f6798deebfbd7511fe8181336ef7064
ThePdrXx <pdrpvp0310@gmail.com>
2023-03-01 11:13:21 -0300

    feat: Add core components

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    size: 'md',
+    children:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae perspiciatis, voluptatibus amet quae vitae blanditiis? Totam rem placeat eveniet minima pariatur dolorum consectetur quibusdam distinctio, animi consequuntur nemo ut vitae.',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '3xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
